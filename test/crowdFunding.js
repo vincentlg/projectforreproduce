8c6c90d0451fa3862fdd7c41107fc58d700b277e
Vincent Le Gallic <legallic.vincent@gmail.com>
2017-05-21 18:30:37 +0200

    bugfix

--- test/crowdFunding.js
+++ test/crowdFunding.js
@@ -4,6 +4,8 @@ contract('CrowdFunding', function(accounts) {
 
   var account_one = accounts[0];
   var account_two = accounts[2];
+  var campaign;
+  var id;
 
     it("should create a new campaign", function() {
       return CrowdFunding.deployed().then(function(instance) {
@@ -15,10 +17,10 @@ contract('CrowdFunding', function(accounts) {
         })
         .then(function() {
           assert.equal(web3.eth.getBalance(campaign.address).toNumber(), 1, "Balance isn't 1 after one contribution of 1");
-          return instance.checkGoalReached.call(id);
+          return campaign.checkGoalReached.call(id);
         })
         .then(function(reached) {
-            assert.equal(reached, false, "Campaign with goal 2 is reached with 1");
+            assert.equal(reached, false, "Campaign with goal 2 is reached with balance 1");
         });
     });
   });
